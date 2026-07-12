cc8c114553ca1ce339590535e0f070a28d5be993
soap155-dotcom <soap155@gmail.com>
2026-07-12 14:38:01 +0900

    chore: create project structure

--- src/app.js
+++ src/app.js
@@ -4,8 +4,23 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+// JSONを受け取る
+app.use(express.json());
+
+// 動作確認
 app.get("/", (req, res) => {
   res.send("Lumos LINE Bot is running.");
+});
+
+// Webhook
+app.post("/webhook", (req, res) => {
+
+  console.log("========== WEBHOOK ==========");
+  console.log(req.body);
+  console.log("=============================");
+
+  res.sendStatus(200);
+
 });
 
 app.listen(PORT, () => {
